1a415c53e866f00852ce5b6ba47f611a7c5e1e0d
YuRMyDestiny <182493478+YuRMyDestiny@users.noreply.github.com>
2025-02-17 20:34:08 +0000

    Change grammar

--- src/membership_management.sql
+++ src/membership_management.sql
@@ -17,7 +17,15 @@ WHERE status = 'Active';
 
 -- 2. Calculate the average duration of gym visits for each membership type
 -- TODO: Write a query to calculate the average duration of gym visits for each membership type
-
+SELECT type AS membership_type, AVG(unixepoch(check_out_time) - unixepoch(check_in_time))/60 AS avg_visit_duration_minutes
+FROM attendance
+JOIN memberships ON attendance.member_id = memberships.member_id
+GROUP BY membership_type;
 
 -- 3. Identify members with expiring memberships this year
 -- TODO: Write a query to identify members with expiring memberships this year
+SELECT members.member_id, first_name, last_name, email, end_date
+FROM memberships 
+JOIN members ON members.member_id = memberships.member_id
+WHERE strftime('%Y', end_date) = strftime('%Y', 'now');
+-------   CHECK ENGLISH  ----------------
